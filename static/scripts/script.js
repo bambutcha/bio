07c1b60f9e1e53ba727bb3e7a06fc4f5cf9e921a
bambutcha <stringdeity@gmail.com>
2024-08-21 21:32:44 +0300

    Complete soundButton functional
    [#dev]

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,7 +1,41 @@
 function scrollToBlock(blockId) {
-    const targetBlock = document.getElementById(blockId);
-    targetBlock.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    });
-  }
+  const targetBlock = document.getElementById(blockId);
+  targetBlock.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start'
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  var audio = document.getElementById('notif');
+  var soundButton = document.getElementById('soundButton');
+  var clickCount = 0;
+
+  soundButton.addEventListener('click', function() {
+    clickCount++;
+    if (clickCount === 1) {
+      audio.play();
+      soundButton.classList.add('play');
+    } else if (clickCount === 2) {
+      audio.pause();
+      soundButton.classList.remove('play');
+      audio.currentTime = 0;
+    } else if (clickCount === 3) {
+      audio.currentTime = 0;
+      audio.play();
+      soundButton.classList.add('play');
+      clickCount = 1;
+    }
+  });
+
+  audio.addEventListener('ended', function() {
+    soundButton.classList.remove('play');
+    clickCount = 0; // Сбрасываем счетчик нажатий
+  });
+});
+
+
+  
+  
+  
+  
